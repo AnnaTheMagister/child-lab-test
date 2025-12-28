be4fb1f622305a0307e6020d0873e4eabc40c642
Anna Landa <alanda@samokat.ru>
2025-12-28 13:30:03 +0300

    CHI-41 исправила название темы

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd163fd5f9752314def24');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'db61f39f3a3a69f48c4f');
