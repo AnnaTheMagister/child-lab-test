5ecb0dd16e7a3961d0229b1d01e26c0eb687f719
Anna Landa <alanda@samokat.ru>
2026-01-09 07:09:57 +0300

    CHI-35 добавлена страница 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+
+get_header(); ?>
+
+<div class="container not-found-page">
+    <h1>Ой! Страница не найдена!</h1>
+    <p>К сожалению, мы не можем найти запрошенную вами страницу</p>
+</div>
+
+<?php get_footer(); ?>
