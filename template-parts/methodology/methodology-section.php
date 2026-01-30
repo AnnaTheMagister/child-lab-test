aa114293ec9b83e126080ac3d31e0be8f5b4d0db
Anna Landa <alanda@samokat.ru>
2026-01-30 22:31:34 +0300

    переверстала страницу методологии

--- template-parts/methodology/methodology-section.php
+++ template-parts/methodology/methodology-section.php
@@ -1,18 +1,29 @@
+<?php $page_data = get_post_by_slug('methodology', 'page'); ?>
 <div class="container">
     <div class="childlab-widget methodology">
         <div class="row">
             <?php $term_data = get_methodology_data_for_page(); ?>
             <?php $empty_placeholder = '<div class="empty-placeholder">' . esc_html__('Выберите элемент на дереве, чтобы прочитать о нём подробнее', 'childlab') . '</div>'; ?>
+
+            <div
+                class="col-lg-12 col-md-12  order-md-2 order-lg-1 d-sm-none s-xs-none d-md-block d-lg-block d-xlg-block d-xxlg-block">
+                <div class="methodology__header">
+                    <h1><?php esc_html_e("Методология", "childlab") ?></h1>
+                    <p><?php esc_html_e('Наша методология «дерева» отражает то, что способности не появляются сами по себе — они вырастают из отношений и активной деятельности ребёнка', 'childlab') ?>
+                    </p>
+                </div>
+            </div>
+
             <?php if (!$term_data): ?>
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 order-xs-2 order-sm-2 order-md-2 order-lg-1">
+                <div
+                    class="col-sm-12 col-xs-12  order-sm-2 order-xs-2 d-sm-block s-xs-block d-md-none d-lg-none d-xlg-none d-xxlg-none">
                     <div class="methodology__header">
-                        <?php
-                        $content = apply_filters('the_content', get_post_by_slug('methodology', 'page')->post_content);
-                        echo $content;
-                        ?>
-                        <div class="d-md-block d-lg-none">
-                            <?php echo $empty_placeholder ?>
-                        </div>
+                        <h1><?php esc_html_e("Методология", "childlab") ?></h1>
+                        <p><?php esc_html_e('Наша методология «дерева» отражает то, что способности не появляются сами по себе — они вырастают из отношений и активной деятельности ребёнка', 'childlab') ?>
+                        </p>
+
+
+                        <?php echo $empty_placeholder ?>
                     </div>
                 </div>
             <?php endif; ?>
@@ -36,9 +47,16 @@
             <?php endif; ?>
             <?php if ($term_data): ?>
                 <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12 order-xs-3 order-sm-3 order-md-3 order-lg-3">
-                    <div class="childlab-widget methodology__description">
-                        <?php get_template_part('template-parts/methodology/tag-description'); ?>
-                    </div>
+
+
+                    <?php $term = get_methodology_data_for_page();
+
+                    if ($term && $term->term_id) {
+                        echo get_articles_list_by_taxonomy('methodology_tag', $term->term_id, esc_html__("Статьи по теме", 'childlab'));
+                    }
+                    ?>
+
+
                 </div>
             <?php endif; ?>
 
@@ -46,11 +64,13 @@
     </div>
 
 </div>
-<div class="container">
-    <?php $term = get_methodology_data_for_page();
 
-    if ($term && $term->term_id) {
-        echo get_articles_list_by_taxonomy('methodology_tag', $term->term_id, esc_html__("Статьи по теме", 'childlab'));
-    }
-    ?>
+
+<div class="container mt-3">
+    <div class="childlab-widget">
+        <?php
+        $content = apply_filters('the_content', get_the_content($page_data->ID));
+        echo $content;
+        ?>
+    </div>
 </div>
