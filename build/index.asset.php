6aee35fef2ad4d2526172fc62d636225d5d847fa
Anna Landa <alanda@ecom.tech>
2026-02-15 17:05:26 +0300

    Добавила переводы

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '007c87ede0e6c0d6b68b');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '059ec7c0428d9323d08c');
